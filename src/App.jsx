9e9c005c1368ae95c9a9faa0082dc4c44b0538f7
topurayhan007 <topurayhan007@gmail.com>
2023-05-10 15:30:15 +0600

    initial setup, form

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+  };
+
+  return (
+    <>
+      <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" name="name" id="" />
+      </form>
+    </>
+  );
+}
+
+export default App;
